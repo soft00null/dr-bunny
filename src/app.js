71215771bc12ce27e920d182a31d02080ab971c7
Mahesh Patil <soft00null@users.noreply.github.com>
2018-03-28 11:36:37 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -705,8 +705,9 @@ class FacebookBot {
                                                                 parameters: JSON.stringify({name: "Clément"})})
                                                              .then(function(engagement) { 
 
-
-                                                               console.log(JSON.stringify(engagement.context.widgets.gather_1.Digits)); 
+                                                                console.log('Twilio Engagement :'+engagement.sid); 
+                                                                console.log(JSON.stringify(engagement.context)); 
+                                                                console.log(JSON.stringify(engagement.context.widgets.gather_1.Digits)); 
 
                                                                //sending back response to user           
                                                                let alert_1 = "Ambulance is dispatched, following are details (1)Ambulance number : "+ambulance_number+", (2)Driver Name : "+driver_name+", (3)Driver Mobile : "+driver_mobile;                  
